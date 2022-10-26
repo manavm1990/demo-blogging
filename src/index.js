96a49a269668bb07dc7988598263d16a10fec428
Manav Misra <manavm@visionwebsoft.com>
2022-10-25 21:11:52 -0500

    Publish the blog post form the form

--- src/index.js
+++ src/index.js
@@ -5,3 +5,20 @@ const h1 = document.createElement("h1");
 const h2 = document.createElement("h2");
 const small = document.createElement("small");
 const p = document.createElement("p");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const fd = new FormData(event.target);
+  const values = Object.fromEntries(fd.entries());
+
+  // Object Destructuring
+  const { title, subtitle, author, content } = values;
+
+  h1.textContent = title;
+  h2.textContent = subtitle;
+  small.textContent = author;
+  p.textContent = content;
+
+  main.append(h1, h2, small, p);
+});
